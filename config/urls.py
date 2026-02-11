654775ca8c1d6cdb80594537dd3c38e01db0230b
waseem-abbas786 <waseemabbaskpr786@gmail.com>
2026-02-11 11:54:01 +0500

    adding swagger and ngrok

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,17 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('App.esg.urls')),
+    path('', include('App.esg.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
